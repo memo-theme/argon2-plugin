ad359b521d53caa2a0feb87018a941588efe8773
theme-memo <245191659+theme-memo@users.noreply.github.com>
2025-11-27 13:04:57 +0000

    fix(stylelint): Disable media-feature-range-notation rule

---
+++ stylelint.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import("stylelint").Config} */
+const config = {
+  extends: ["stylelint-config-standard"],
+  rules: {
+    "media-feature-range-notation": null,
+  },
+};
+export default config;
