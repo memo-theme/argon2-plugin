6f14e9062b6a40b52ebfb5081aa15092a6f39245
theme-memo <245191659+theme-memo@users.noreply.github.com>
2025-11-27 16:57:39 +0000

    feat(pages): Force static rendering for homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 
+export const dynamic = "force-static";
+
 export default function Home() {
   return (
     <div className={styles.page}>
@@ -61,6 +63,50 @@ export default function Home() {
           </a>
         </div>
       </main>
+      <footer className={styles.footer}>
+        <a
+          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/file.svg"
+            alt="File icon"
+            width={16}
+            height={16}
+          />
+          Learn
+        </a>
+        <a
+          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/window.svg"
+            alt="Window icon"
+            width={16}
+            height={16}
+          />
+          Examples
+        </a>
+        <a
+          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/globe.svg"
+            alt="Globe icon"
+            width={16}
+            height={16}
+          />
+          Go to Vercel
+        </a>
+      </footer>
     </div>
   );
 }
